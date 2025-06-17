57a9e6c7b94d0b701da3012bc7742a5cfc960940
daniel_shvarts <daniel04sh@gmail.com>
2025-06-17 21:10:55 +0300

    add async await

--- src/core/tests.ts
+++ src/core/tests.ts
@@ -2,24 +2,24 @@ import { test } from "vitest";
 
 export const setUpSkippedTests = <T>(instance: T, skippedTests: Set<string>) => {
     for (const testName of skippedTests) {
-        test.skip(testName, () => {
-            instance[testName]();
+        test.skip(testName, async () => {
+            await instance[testName]();
         });
     }
 }
 
 export const setUpOnlyTests = <T>(instance: T, onlyTests: Set<string>) => {
     for (const testName of onlyTests) {
-        test.only(testName, () => {
-            instance[testName]();
+        test.only(testName, async () => {
+            await instance[testName]();
         });
     }
 }
 
 export const setUpTests = <T>(instance: T, tests: Set<string>) => {
     for (const testName of tests) {
-        test(testName, () => {
-            instance[testName]();
+        test(testName, async () => {
+            await instance[testName]();
         });
     }
 }
