fc0c78322147689dd66302a0cddebb2280d88aef
daniel_shvarts <daniel04sh@gmail.com>
2025-06-19 14:16:05 +0300

    fix hooks bug

--- src/core/hooks.ts
+++ src/core/hooks.ts
@@ -1,10 +1,10 @@
 import { beforeEach, afterEach, beforeAll, afterAll } from "vitest";
 
 type Hooks = {
-    beforeAlls: Set<string>,
-    beforeEachs: Set<string>,
-    afterEachs: Set<string>,
-    afterAlls: Set<string>,
+    beforeAlls: string[],
+    beforeEachs: string[],
+    afterEachs: string[],
+    afterAlls: string[],
 };
 
 export const setupHooks = async <T>(instance: T, hooks: Hooks) => {
@@ -12,12 +12,12 @@ export const setupHooks = async <T>(instance: T, hooks: Hooks) => {
         beforeAlls, beforeEachs, afterEachs, afterAlls
     } = hooks;
 
-    if (beforeEachs.size)
+    if (beforeEachs.length)
         beforeEach(async () => beforeEachs.forEach(async (m) => await instance[m]()));
-    if (afterEachs.size)
+    if (afterEachs.length)
         afterEach(async () => afterEachs.forEach(async (m) => await instance[m]()));
-    if (beforeAlls.size)
+    if (beforeAlls.length)
         beforeAll(async () => beforeAlls.forEach(async (m) => await instance[m]()));
-    if (afterAlls.size)
+    if (afterAlls.length)
         afterAll(async () => afterAlls.forEach(async (m) => await instance[m]()));
 }
